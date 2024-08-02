47203f38e302468654fa158a79b29e182ab22249
Nagarjuna0033 <nagarjunabanda4@gmail.com>
2024-08-02 18:58:51 +0530

    Updated links

--- src/components/Testimonals.js
+++ src/components/Testimonals.js
@@ -39,82 +39,85 @@ export default function Testimonals() {
         : testimonials && prevItemLength - 1;
     });
   };
-
   return (
     <>
-      <div className="testimonial-container">
-        <div className="btn" id="btn-prev" onClick={prev}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 320 512"
-            style={{ transform: "rotate(180deg)" }}
-          >
-            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-          </svg>
-        </div>
-        <div className="btn" id="btn-next" onClick={next}>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-          </svg>
-        </div>
-        {testimonials
-          ? testimonials.map((item, index) => {
-              if (index === itemLength) {
+      {testimonials && testimonials.length > 0 ? (
+        <div className="testimonial-container">
+          <div className="btn" id="btn-prev" onClick={prev}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
+              style={{ transform: "rotate(180deg)" }}
+            >
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+            </svg>
+          </div>
+          <div className="btn" id="btn-next" onClick={next}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+            </svg>
+          </div>
+          {testimonials
+            ? testimonials.map((item, index) => {
+                if (index === itemLength) {
+                  return (
+                    <>
+                      <p className="testimonial">{item.message}</p>
+                      <div className="user">
+                        <img
+                          src={"data:image/jpeg;base64," + item.photo}
+                          alt="user"
+                          className="user-image"
+                        />
+                        <div className="user-details">
+                          <h4 className="username">{item.name}</h4>
+                          <p className="role">{item.designation}</p>
+                        </div>
+                      </div>
+                    </>
+                  );
+                }
+              })
+            : Array.from({ length: 1 }).map((_, index) => {
                 return (
                   <>
-                    <p className="testimonial">{item.message}</p>
+                    <p className="testimonial">
+                      <Skeleton
+                        variant="text"
+                        sx={{ backgroundColor: "gray", width: "9rem" }}
+                      />
+                    </p>
                     <div className="user">
-                      <img
-                        src={"data:image/jpeg;base64," + item.photo}
-                        alt="user"
-                        className="user-image"
+                      <Skeleton
+                        variant="circular"
+                        width={"5rem"}
+                        height={"5rem"}
+                        sx={{
+                          backgroundColor: "gray",
+                        }}
                       />
                       <div className="user-details">
-                        <h4 className="username">{item.name}</h4>
-                        <p className="role">{item.designation}</p>
+                        <h4 className="username">
+                          <Skeleton
+                            variant="text"
+                            sx={{ backgroundColor: "gray", width: "9rem" }}
+                          />
+                        </h4>
+                        <p className="role">
+                          <Skeleton
+                            variant="text"
+                            sx={{ backgroundColor: "gray", width: "9rem" }}
+                          />
+                        </p>
                       </div>
                     </div>
                   </>
                 );
-              }
-            })
-          : Array.from({ length: 1 }).map((_, index) => {
-              return (
-                <>
-                  <p className="testimonial">
-                    <Skeleton
-                      variant="text"
-                      sx={{ backgroundColor: "gray", width: "9rem" }}
-                    />
-                  </p>
-                  <div className="user">
-                    <Skeleton
-                      variant="circular"
-                      width={"5rem"}
-                      height={"5rem"}
-                      sx={{
-                        backgroundColor: "gray",
-                      }}
-                    />
-                    <div className="user-details">
-                      <h4 className="username">
-                        <Skeleton
-                          variant="text"
-                          sx={{ backgroundColor: "gray", width: "9rem" }}
-                        />
-                      </h4>
-                      <p className="role">
-                        <Skeleton
-                          variant="text"
-                          sx={{ backgroundColor: "gray", width: "9rem" }}
-                        />
-                      </p>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-      </div>
+              })}
+        </div>
+      ) : (
+        ""
+      )}
     </>
   );
 }
