e6c613ef5496266af6db1f1115f3996b9745c5bd
Nagarjuna0033 <nagarjunabanda4@gmail.com>
2024-08-02 15:59:30 +0530

    Added dependencies

--- src/components/Events.js
+++ src/components/Events.js
@@ -124,7 +124,7 @@ export default function Events() {
                       height={"21.9rem"}
                       sx={{
                         backgroundColor: "gray",
-                        display:"inline-block"
+                        display: "inline-block",
                       }}
                     />
                   </div>
@@ -137,7 +137,7 @@ export default function Events() {
                           height={"4rem"}
                           sx={{
                             backgroundColor: "gray",
-                            display:"inline-block"
+                            display: "inline-block",
                           }}
                         />
                       </h2>
@@ -151,7 +151,7 @@ export default function Events() {
                             height={"2rem"}
                             sx={{
                               backgroundColor: "gray",
-                              display:"inline-block"
+                              display: "inline-block",
                             }}
                           />
                         </h2>
@@ -159,7 +159,7 @@ export default function Events() {
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 512 512"
-                            style={{marginTop:"1rem"}}
+                            style={{ marginTop: "1rem" }}
                           >
                             <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
                           </svg>
@@ -169,8 +169,7 @@ export default function Events() {
                             height={"2rem"}
                             sx={{
                               backgroundColor: "gray",
-                              display:"inline-block",
-                               
+                              display: "inline-block",
                             }}
                           />
                         </span>
@@ -188,7 +187,7 @@ export default function Events() {
                             sx={{
                               // marginTop:"-4rem",
                               backgroundColor: "gray",
-                              display:"inline-block"
+                              display: "inline-block",
                             }}
                           />
                         </span>
@@ -202,21 +201,21 @@ export default function Events() {
                               height={"8rem"}
                               sx={{
                                 backgroundColor: "gray",
-                                display:"inline-block", 
-                                margin:"0px"
-                             }}
+                                display: "inline-block",
+                                margin: "0px",
+                              }}
                             />
                           </p>
                         </div>
 
-                          <Skeleton
-                            variant="text"
-                            width={"6rem"}
-                            height={"2rem"}
-                            sx={{
-                              backgroundColor: "gray",
-                            }}
-                          />
+                        <Skeleton
+                          variant="text"
+                          width={"6rem"}
+                          height={"2rem"}
+                          sx={{
+                            backgroundColor: "gray",
+                          }}
+                        />
                       </div>
                     </div>
                   </div>
